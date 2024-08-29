b9af0f3905a7ac9a03b31c22292991c12574bb5a
AndreSuryana <Andresuryana17@gmail.com>
2024-08-29 16:15:03 +0700

    fix: Handle Redis errors gracefully
    Details:
    - When a Redis connection error occurs or the retrieved data is corrupted, the service will now attempt to fetch the data again from the third-party API
    - If writing to the Redis cache fails, the error will be ignored, allowing the service to continue without interruption

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -10,41 +10,44 @@ const getForecastData = async (location) => {
     // Cache key based on location
     const cacheKey = `weather:${location}`
 
+    // Check if data is cached
     try {
-        // Check if data is cached
         const cachedData = await redisClient.get(cacheKey)
         if (cachedData) {
             logger.info(`Cache hit for location: ${location}. Data retrieved successfully from Redis.`)
             return JSON.parse(cachedData)
         }
+    } catch (err) {
+        logger.error(`Error retrieving key ${cacheKey}:`, err)
+    }
 
-        // Fetch data from the third-party API
-        const response = await httpClient.get('forecast', {
-            params: {
-                locations: location,
-                locationMode: 'single', // Single location
-                forecastDays: 7, // Week
-                aggregateHours: 24,
-                unitGroup: 'metric',
-                contentType: 'json',
-                key: process.env.WEATHER_API_KEY
-            }
-        })
+    // Fetch data from the third-party API
+    const response = await httpClient.get('forecast', {
+        params: {
+            locations: location,
+            locationMode: 'single', // Single location
+            forecastDays: 7, // Week
+            aggregateHours: 24,
+            unitGroup: 'metric',
+            contentType: 'json',
+            key: process.env.WEATHER_API_KEY
+        }
+    })
 
-        // Reformat raw data from VisualCrossing Weather API to more compact format
-        const weatherData = getCompactForecastData(response.data)
-        logger.info(`Data retrieved from Third-party API for location: ${location}`)
+    // Reformat raw data from VisualCrossing Weather API to more compact format
+    const weatherData = getCompactForecastData(response.data)
+    logger.info(`Data retrieved from Third-party API for location: ${location}`)
 
-        // Cache the response with the location as the key!
-        await redisClient.set(cacheKey, JSON.stringify(weatherData, null, 2), {
+    // Cache the response with the location as the key!
+    try {
+        await redisClient.set(cacheKey, JSON.stringify(weatherData), {
             EX: EXPIRED_CACHE_TIME
         })
-
-        return weatherData
     } catch (err) {
-        logger.error(`Error fetching weather data for location: ${location} - ${err.message}`)
-        throw err // Rethrow the error to be handled by the caller
+        logger.error(`Error to put key ${cacheKey}:`, err)
     }
+
+    return weatherData
 }
 
 module.exports = { getForecastData }
